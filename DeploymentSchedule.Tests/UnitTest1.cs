e6f853e9187ecd74bd1ff87cf74a0f3ab9130be1
Ameenah_Oduola <aoduola@appzonegroup.com>
2019-08-27 17:12:53 +0100

    Updated Unit Test

--- DeploymentSchedule.Tests/UnitTest1.cs
+++ DeploymentSchedule.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DeploymentSchedule.Logic;
+using DeploymentSchedule.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DeploymentSchedule.Tests
@@ -6,25 +8,41 @@ namespace DeploymentSchedule.Tests
     [TestClass]
     public class UnitTest1
     {
+        //method to test Create and GetByName method
         [TestMethod]
         public void TestCreate()
         {
-
+            var deployment = new Deployment
+            {
+                Name = "TestCreate",
+                DeploymentDuration = "2 hours",
+                Description = "Test Deployment",
+                IssuesEncoutered = "None yet",
+                ScheduleTime = new DateTime(2019, 9, 1),
+                Status = "To Do"
+            };
+            var logic = new DeploymentLogic();
+            logic.Create(deployment);
+            Assert.IsNotNull(logic.GetByName(deployment.Name));
         }
+        //Method to test update logic
         [TestMethod]
         public void TestUpdate()
         {
-
+            var logic = new DeploymentLogic();
+            var deployment = logic.GetByName("TestCreate");
+            deployment.Description = "Update Test Deployment";
+            logic.Update(deployment, "TestCreate");
+            var updatedDeployment = logic.GetByName("TestCreate");
+            Assert.Equals(deployment.Description, updatedDeployment.Description);
         }
-        [TestMethod]
-        public void TestGet()
-        {
-
-        }
+        //Method to test delete logic
         [TestMethod]
         public void TestDelete()
         {
-
+            var logic = new DeploymentLogic();
+            logic.Delete("TestCreate");
+            Assert.IsNull(logic.GetByName("TestCreate"));
         }
     }
 }
